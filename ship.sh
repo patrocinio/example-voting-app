e571dd1dbef69247dc3f8ebdd6e6dfdd0d76098a
Eduardo Patrocinio <epatro@gmail.com>
2016-11-23 15:56:05 -0500

    Change ship step

--- ship.sh
+++ ship.sh
@@ -1,3 +1,3 @@
-docker push vote_vote
-docker push vote_result
-docker push vote_worker
+docker push patrocinio/vote_vote:test
+docker push patrocinio/vote_result:test
+docker push patrocinio/vote_worker:test
