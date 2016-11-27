083b886f0baa2fe29b3490d00f17b0182c47b5ca
Eduardo Patrocinio <epatro@gmail.com>
2016-11-27 12:33:15 -0500

    Trigger Jenkins

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 cd vote
-sudo docker build -t patrocinio/vote_vote:test .
+docker build -t patrocinio/vote_vote:test .
 cd ../result
-sudo docker build -t patrocinio/vote_result:test .
+docker build -t patrocinio/vote_result:test .
 cd ../worker
-sudo docker build -t patrocinio/vote_worker:test .
+docker build -t patrocinio/vote_worker:test .
 
