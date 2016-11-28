8b5a9d164ecc2a7bd628beb63ba3e865c585a880
Eduardo Patrocinio <epatro@gmail.com>
2016-11-28 13:59:59 -0500

    BlackWhite

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Pink")
-option_b = os.getenv('OPTION_B', "Blue")
+option_a = os.getenv('OPTION_A', "Black")
+option_b = os.getenv('OPTION_B', "White")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
