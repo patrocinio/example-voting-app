5cf6032b9193a1571a7e8a755e255868576b6db9
Eduardo Patrocinio <epatro@gmail.com>
2016-11-29 07:09:43 -0500

    Tag the image with an environment variable

--- ship.sh
+++ ship.sh
@@ -1,4 +1,11 @@
+# Push the images
 docker login -u patrocinio -p Boch3ch4
 docker push patrocinio/vote_vote:test
 docker push patrocinio/vote_result:test
 docker push patrocinio/vote_worker:test
+
+# Remove the images
+docker rmi patrocinio/vote_vote:test
+docker rmi patrocinio/vote_result:test
+docker rmi patrocinio/vote_worker:test
+
