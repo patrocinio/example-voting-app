6b7f17970e84a4fb62d7767287285c911da0fbd5
Eduardo Patrocinio <epatro@gmail.com>
2016-11-29 22:32:54 -0500

    SouthAmerica

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Tikka")
-option_b = os.getenv('OPTION_B', "Masala")
+option_a = os.getenv('OPTION_A', "Colombia")
+option_b = os.getenv('OPTION_B', "Venezuela")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
