30d61ee59e8d369577f51be7e09d1e30e641ebc5
Rick Osowski <rosowski@gmail.com>
2016-12-02 17:18:29 -0600

    Reset voting options for PR
    Signed-off-by: Rick Osowski <rosowski@gmail.com>

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Coke")
-option_b = os.getenv('OPTION_B', "Pepsi")
+option_a = os.getenv('OPTION_A', "Hacking")
+option_b = os.getenv('OPTION_B', "Exposed")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
