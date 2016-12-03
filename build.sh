dbcea730af2ecbb7736b2620d0819e8a3e89ff72
Eduardo Patrocinio <epatro@gmail.com>
2016-12-03 07:55:27 -0500

    Version the image according to the build number

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 cd vote
-docker build -t patrocinio/vote_vote:test .
+docker build -t patrocinio/vote_vote:test-${BUILD_NUMBER} .
 cd ../result
-docker build -t patrocinio/vote_result:test .
+docker build -t patrocinio/vote_result:test-${BUILD_NUMBER} .
 cd ../worker
-docker build -t patrocinio/vote_worker:test .
+docker build -t patrocinio/vote_worker:test-${BUILD_NUMBER} .
 
