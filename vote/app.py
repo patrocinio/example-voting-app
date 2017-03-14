5a0128efaf88bb57c3592b5df90bc6c013164671
Eduardo Patrocinio <epatro@gmail.com>
2017-03-14 08:19:44 -0400

    Interconnect 2

--- vote/app.py
+++ vote/app.py
@@ -6,7 +6,7 @@ import random
 import json
 
 option_a = os.getenv('OPTION_A', "Inter")
-option_b = os.getenv('OPTION_B', "Connect")
+option_b = os.getenv('OPTION_B', "ConnectX")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
