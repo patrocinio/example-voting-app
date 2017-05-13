4411e597fb550bca424fd8d8d46e934818735d83
Eduardo Patrocinio <epatro@gmail.com>
2017-05-13 09:28:12 -0400

    Blue vs. Green

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Inter")
-option_b = os.getenv('OPTION_B', "ConnectZ")
+option_a = os.getenv('OPTION_A', "Blue")
+option_b = os.getenv('OPTION_B', "Green")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
